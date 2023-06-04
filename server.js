a2b50fffc3951d693f9c8e3fd30bf7927c557b63
Roadglide131 <darrenj@oldschoolrenovation.com>
2023-06-04 15:31:46 -0500

    Connected backend to front end

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.get("/api/notes", (req, res) => {
   const db = fs.readFileSync("./db/db.json");
